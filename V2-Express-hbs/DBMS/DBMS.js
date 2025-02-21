305e69b29be6b938fc230fd0a3a7b665035ad9ee
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-21 12:48:15 +0200

    dbms now routing

--- V2-Express-hbs/DBMS/DBMS.js
+++ V2-Express-hbs/DBMS/DBMS.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from 'dotenv'; dotenv.config({ path: '../.env' });
-import { DBconnect } from "./helpers/mongoHelper.js";
+import { DBconnect } from "./src/helpers/mongoHelper.js";
+import router from "./src/router.js";
 
 const app = express();
 const port = 59534
 
 await DBconnect();
+
+
+
+app.use(`/`, router);
 
 
 app.listen(port, console.log(`Server's running at http://localhost:${port}`));
