5f72a932205e108abed47a3d0f17b96b824daea6
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-22 17:57:59 +0200

    New route to get the whole members data

--- V2-Express-hbs/DBMS/src/controllers/memberCtrl.js
+++ V2-Express-hbs/DBMS/src/controllers/memberCtrl.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Member from "../Models/Member.js"
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
+import validateToken from "../helpers/authToken.js";
 
 dotenv.config({ path: '../../.env' });
 const JWT_SECRET = process.env.JWT_SECRET;
@@ -10,31 +10,34 @@ const memberCtrl = Router();
 
 
 memberCtrl.post(`/member`, onPost);
-// memberCtrl.get(`/member/:id`, onGet);
-// memberCtrl.get(`/member/list`, onGetList)
+memberCtrl.get(`/member/list`, onGetList)
+memberCtrl.get(`/member/:id`, onGet);
+
+async function onGet(req, res) {
+    const id = req.params.id;
+
+    try {
+        const search = await Member.findById(id);
+        res.status(200).json(search);
+        
+    } catch (error) {
+        res.status(404).json({msg:`Entry's missing.`})
+    }
+}
 
 
 async function onPost(req, res) {
-    let Authorization;
-    try {
-        Authorization = jwt.verify(req.headers[`in-auth`], JWT_SECRET);
-        
-    } catch (error) {
-        Authorization = undefined
-    }
-    
+    const Authorization = validateToken(req.headers[`in-auth`], JWT_SECRET);
+
     if(!Authorization) return res.status(401).json({403: `Auth Error`});
-    console.log(`Autorized! \n ${Authorization}`);
-    
+    console.log(`Authorized!`);
     
     const mem = Authorization
     
     try {
-        const exist = await Member.findById(mem._id);
-
+        const exist = await Member.findOne({ blizID: mem.blizID });
         if (exist){
             await Member.findByIdAndUpdate(mem._id, mem);
-            
         }else {
             const memTry = new Member(mem);
             await memTry.save();
@@ -46,5 +49,12 @@ async function onPost(req, res) {
         
     }   
 }
-
+async function onGetList(req,res) {
+    try {
+        const memList = await Member.find();
+        res.status(200).json(memList);
+    } catch (error) {
+        res.status(404).json({msg:`There's no such collection`});
+    }
+}
 export default memberCtrl;
