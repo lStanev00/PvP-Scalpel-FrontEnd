e2a1593212de374f0a7568e47d12b863cc75dd1c
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-24 16:11:58 +0200

    Basic app overview

--- V3-React-Vite-Express/front-end/src/App.jsx
+++ V3-React-Vite-Express/front-end/src/App.jsx
@@ -1,9 +1,33 @@
-import NavMenu from "./components/nav";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navigation from "./components/Router";
+import Home from "./components/home";
 
 function App() {
     return (
         <>
-            <NavMenu />     
+            <Router>
+                <div className="page-wrapper">
+                    <header className="header">
+                        <div className="logo">
+                            <img className="logo-img" src="./logo/logo_resized.png" alt="logo pic" />
+                            PvP Scalpel
+                        </div>
+                        <Navigation />
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path='/' element={<Home />}></Route>
+                            <Route path='/roster'></Route>
+                            <Route path='/leaderbord'></Route>
+                        </Routes>
+                        
+                    </main>
+
+                    <footer className="footer">
+                        <p>&copy; 2025 Lachezar Stanev. All rights reserved.</p>
+                    </footer>
+                </div>
+            </Router>
         </>
     )
 }
--- V3-React-Vite-Express/front-end/src/main.jsx
+++ V3-React-Vite-Express/front-end/src/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from "react-dom/client";
 import './Styles/main.css';
 import App from "./App";
 
-createRoot(document.querySelector(`body`)).render(<App />)
+createRoot(document.querySelector(`body`)).render(<App />);
