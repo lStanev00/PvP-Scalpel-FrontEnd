c98636686f98e521011146681556fa9d0f3116a6
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-25 13:17:58 +0200

    Home roadmap*

---
+++ V3-React-Vite-Express/front-end/src/modules/home.jsx
@@ -0,0 +1,25 @@
+import '../Styles/home.css'
+export default function Home() {
+    return (
+        <>
+        <section id="home" className="hero">
+        <div className="roadmap">
+            <h2 className="hero-text">Spoiler Alert: The App Is Under Development!</h2>
+            <p>Actively working on bringing this app to life. Here's the priority roadmap:</p>
+            <ol>
+                <li><strong>Server-Side Data Integration:</strong> Fetch data from Blizzard's API.</li>
+                <li><strong>Server-Side Data Storage:</strong> Efficiently store and manage data.</li>
+                <li><strong>Achievements Storage Optimization:</strong> Improve how achievements are stored and retrieved.</li>
+                <li><strong>Basic User Interface:</strong> Establish a foundational UI for the site.</li>
+                <li><strong>Leaderboard Optimization:</strong> Enhance leaderboard functionality and performance.</li>
+                <li><strong>Roster Banner:</strong> Add a roster banner with links to personal character pages.</li>
+                <li><strong>Personal Character Pages:</strong> Create detailed pages for individual characters.</li>
+                <li><strong>Enhanced Functionality:</strong> Expand and refine app features.</li>
+                <li><strong>User-Friendly Interface:</strong> Build an intuitive and engaging UI for users.</li>
+                <li><strong>Seamless Navigation:</strong> Ensure smooth and user-friendly navigation throughout the site.</li>
+            </ol>
+        </div>
+    </section>
+        </>
+    )
+}
