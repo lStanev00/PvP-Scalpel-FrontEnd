3eae0d68d8bb149d79a2d9b0519216c91b8b9ce2
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-25 15:42:34 +0200

    New searchBox defenition + logic

---
+++ V3-React-Vite-Express/front-end/src/components/searchBox.jsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+export default function SearchBox({ data, setSearch }) {
+    const [query, setQuery] = useState(``);
+
+    const onClick = () => {
+        document.getElementById(`searchInput`).value = ``;
+        setQuery(``)
+    }
+
+    useEffect(() => {
+        const onInput = (event) => {
+            const searchValue = event.target.value;
+            setQuery(searchValue.toLowerCase());
+        }
+
+        const inputEl = document.getElementById(`searchInput`);
+        inputEl.addEventListener(`input`, onInput);
+
+        return () => {
+            inputEl.removeEventListener(`input`, onInput);
+        }
+    }, []);
+
+    useEffect(() => {
+        setSearch(data.filter(char => {
+            return char.name.toLowerCase().includes(query)
+        }))
+    }, [query])
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          id="searchInput"
+          className="search-input"
+          placeholder="Search for a character..."
+        />
+        <button onClick={onClick} id="searchBtn" className="search-btn">
+          Clear
+        </button>
+      </div>
+    </>
+  );
+}
