029bbb8c4585df9afacdb69b01b28e3d37e0fc12
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-02-28 11:50:17 +0200

    Pagination Logic now working

--- V3-React-Vite-Express/front-end/src/components/LDB/paginationContainer.jsx
+++ V3-React-Vite-Express/front-end/src/components/LDB/paginationContainer.jsx
@@ -1,19 +1,49 @@
 import { useState } from "react";
 import paginationStyles from '../../Styles/modular/pagination.module.css';
 
-
 export default function PaginatioContainer ({ data, page ,setPage }) {
     const [pageCounter, setCounter] = useState(1);
+
+    const nextPage = (e) => {
+        setCounter(page => {
+            const newValue = page + 1
+            setPage(data[newValue - 1]);
+            return newValue
+        });
+
+    }
+
+    const lastPage = (e) => {
+        setCounter((data.length));
+        setPage(data[data.length - 1]);
+    }
+
+    const prevPage = (e) => {
+        setCounter(page => {
+            const newValue = page - 1;
+            setPage(data[newValue - 1]);
+            return newValue
+        })
+    }
+
+    const firstPage = (e) => {
+        setCounter(page => {
+            const newValue = 1;
+            setPage(data[0]);
+            return newValue
+        })
+    }
+    
 
     if (!data) return (<></>);
 
     return (
                 <div className={paginationStyles["pagination-container"]}>
-                    <button className={paginationStyles["pagination-btn"]} disabled>« First</button>
-                    <button className={paginationStyles["pagination-btn"]} disabled>‹ Prev</button>
+                    <button onClick={firstPage} className={paginationStyles["pagination-btn"]} disabled = {pageCounter === 1}>« First</button>
+                    <button onClick={prevPage} className={paginationStyles["pagination-btn"]} disabled = {pageCounter === 1}>‹ Prev</button>
                     <span className={paginationStyles["pagination-info"]}>Page {pageCounter} of {data.length}</span>
-                    <button className={paginationStyles["pagination-btn"]}>Next ›</button>
-                    <button className={paginationStyles["pagination-btn"]}>Last »</button>
+                    <button onClick={nextPage} disabled = {pageCounter === (data.length)} className={paginationStyles["pagination-btn"] }>Next ›</button>
+                    <button onClick={lastPage} disabled = {pageCounter === (data.length)} className={paginationStyles["pagination-btn"]}>Last »</button>
                 </div>
     )
 }
