478af6134f0fbc7669643d4589e706a4b3ce2fb7
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-04 06:57:36 +0200

    Setting refer const and passing it to childerens

--- V3-React-Vite-Express/front-end/src/components/LDB/ContentContainer.jsx
+++ V3-React-Vite-Express/front-end/src/components/LDB/ContentContainer.jsx
@@ -2,13 +2,16 @@ import LDBHeaderContent from "./LDBHeader";
 import LDBSearch from "./LDBSearch";
 import TableContent from "./TableContent";
 import PaginatioContainer from "./paginationContainer";
+import { useRef } from "react";
 export default function ContentContainer({ data, content, page, setPage }) {
+    const refs = useRef({});
+
     if (data) {return (
         
         <section className="leaderboard-container" style={{visibility: content ? `visible` : `hidden`}}>
             <LDBHeaderContent content={content} />
-            <LDBSearch data={data} setPage={setPage}/>
-            <TableContent page={page} content={content}/>
+            <LDBSearch data={data} setPage={setPage} refs={refs}/>
+            <TableContent page={page} content={content} refs={refs}/>
             <PaginatioContainer data={data} page={page} setPage={setPage} />
         </section>
 
