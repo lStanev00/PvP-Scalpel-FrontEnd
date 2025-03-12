888c99bbce9bd29b5878ef6551bbf197f8b3dbaa
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-12 10:35:10 +0200

    The requests will be in helper OBJ outside the fn

---
+++ V3-React-Vite-Express/DBMS/helpers/blizFetch-helpers/endpointFetchesBliz.js
@@ -0,0 +1,52 @@
+const helpFetch = {
+    getAccessToken : async function (clientId, clientSecret) {
+        const tokenUrl = 'https://eu.battle.net/oauth/token';
+        
+        try {
+            const response = await fetch(tokenUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    Authorization: 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
+                },
+                body: 'grant_type=client_credentials',
+            });
+    
+            if (!response.ok) {
+                throw new Error(`!! Failed to fetch token: ${response.statusText}`);
+            }
+    
+            const data = await response.json();
+            let token = data.access_token;
+            return token;
+        } catch (error) {
+            console.error('Error fetching access token:', error);
+            throw error;
+        }
+    },
+    getCharProfile: async function (server, realm , name, headers ) {
+        const URI = `https://${server}.api.blizzard.com/profile/wow/character/${realm}/${name}?namespace=profile-${server}&locale=en_US`;
+        try {
+            const data = await (await fetch(URI, headers)).json();
+            return data
+        } catch (error) {
+            console.log(error)
+        }
+
+    },
+    getClassMedia : async function (data, headers) {
+        try {
+            const data1 = await(await fetch(data.character_class.key.href, headers)).json();
+            const data2 = await ( await fetch(data1.media.key.href, headers)).json();
+            return data2 ? data2.assets[0].value : undefined
+            
+        } catch (error) {
+            console.log(error);
+            return undefined
+        }
+
+    }
+}
+
+
+export default helpFetch
--- V3-React-Vite-Express/DBMS/helpers/blizFetch.js
+++ V3-React-Vite-Express/DBMS/helpers/blizFetch.js
@@ -1,41 +1,16 @@
 import dotenv from 'dotenv';
+import helpFetch from './blizFetch-helpers/endpointFetchesBliz.js'
 dotenv.config({ path: '../../.env' });
 
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-
-async function getAccessToken() {
-    const tokenUrl = 'https://eu.battle.net/oauth/token';
-    
-    try {
-        const response = await fetch(tokenUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                Authorization: 'Basic ' + Buffer.from(`${clientId}:${clientSecret}`).toString('base64'),
-            },
-            body: 'grant_type=client_credentials',
-        });
-
-        if (!response.ok) {
-            throw new Error(`!! Failed to fetch token: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        let token = data.access_token;
-        return token;
-    } catch (error) {
-        console.error('Error fetching access token:', error);
-        throw error;
-    }
-}
 
 async function fetchData(server, realm, name) {
     name = name.toLowerCase();
     const result = {};
     let accessToken
     try { // Fetch the access token
-        accessToken = await getAccessToken();
+        accessToken = await helpFetch.getAccessToken(clientId, clientSecret);
     } catch (error) {
         console.log(`Can't fetch token`)
     }
@@ -47,27 +22,10 @@ async function fetchData(server, realm, name) {
           'Pragma': 'no-cache',
         },
     }
-    async function getCharProfile(server, realm , name ) {
-        const URI = `https://${server}.api.blizzard.com/profile/wow/character/${realm}/${name}?namespace=profile-${server}&locale=en_US`;
-        try {
-            const data = await (await fetch(URI, headers)).json();
-            return data
-        } catch (error) {
-            console.log(error)
-        }
-
-    }
-    
-    async function getClassMedia(data, headers) {
-        const data1 = await(await fetch(data.character_class.key.href, headers)).json();
-        const data2 = await ( await fetch(data1.media.key.href, headers)).json();
-        console.log(data2)
-        return data2.assets[0].value
-
-    }
 
     try {
-        let data = await getCharProfile(server, realm, name);
+        // let data = await getCharProfile(server, realm, name);
+        let data = await helpFetch.getCharProfile(server, realm, name, headers);
         result.name = data.name;
         result.blizID = data.id;
         result.level = Number(data.level);
@@ -76,15 +34,7 @@ async function fetchData(server, realm, name) {
         result.achieves[`points`] = Number(data.achievement_points);
         result.class = {}
         result.class.name = data.character_class.name;
-        // console.log(data);
-        // debugger
-        try {
-            result.class.media = await getClassMedia(data, headers);
-            
-        } catch (error) {
-            console.log(error)
-        }
-
+        result.class.media = await helpFetch.getClassMedia(data, headers);
 
     } catch (error) {
         console.log(error)
