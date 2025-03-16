71e39993421b43fbabbfea8585d4ff05b8de7591
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-16 11:02:08 +0200

    Adding route for the new endpoint & feture

--- V3-React-Vite-Express/DBMS/src/router.js
+++ V3-React-Vite-Express/DBMS/src/router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import memberCtrl from "./controllers/memberCtrl.js";
 import LDBController from "./controllers/LDBController.js";
+import characterSearchCTRL from "./controllers/characterSearchCTRL.js";
 
 const router = Router();
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.use(`/`, memberCtrl);
 router.use(`/`, LDBController);
+router.use(`/`, characterSearchCTRL);
 
 export default router
