043053c0b3a944bf4385a5a19a480ca952a53b6a
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-25 21:52:05 +0200

    The reloadBTN is now in outer jsx fn

---
+++ src/components/checkDetails/reloadBTN.jsx
@@ -0,0 +1,44 @@
+import { useParams } from "react-router-dom"
+import { useState } from "react";
+import Style from '../../Styles/modular/charDetails.module.css';
+import timeAgo from "../../helpers/timeAgo.js";
+export default function ReloadBTN({setData, data}) {
+    const { server, realm, name } = useParams();
+    const [isUpdating, setUpdating] = useState(false);
+
+    const patchCharacterData = async () => {
+        const apiEndpoint = `https://api.pvpscalpel.com/patchCharacter/${server}/${realm}/${name}`;
+        setUpdating(state => {return true}); // No waiting set
+        try {
+            const request = await fetch(apiEndpoint, {
+                method: "PATCH"
+            });
+
+            if (!request.ok) return;
+            const requestData = await request.json();
+
+            if (request.status !== 200) return setData({errorMSG : requestData.messege});
+
+            setData(requestData);
+            setUpdating(false)
+        } catch (error) {
+            console.error("Fetch error:", error);
+        }
+    }
+
+    if (!data || !data.updatedAt) return null;
+
+    if (isUpdating) {
+        return (
+            <>
+                <span className={Style["last-updated"]}></span>
+                <button disabled className={Style["button"]}>Updating now . . .</button>
+            </>
+        )
+    }
+    return (<>
+        <span className={Style["last-updated"]}>Last updated: {timeAgo(data.updatedAt)}</span>
+        <button onClick={async () => {await patchCharacterData()}} className={Style["button"]}>{data.nowUpdating ?  "Updating now!" : "Update Now"}</button>
+    </>)
+
+}
