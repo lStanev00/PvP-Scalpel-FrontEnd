0339da422a2182e82cd1490d198b6543c378b80c
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-27 10:54:28 +0200

    Naming conventions covering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navigation from "./components/Router";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import LDB from "./pages/LDB";
-import RosterPage from "./pages/roster";
+import RosterPage from "./pages/Roster";
 import GoToTopButton from "./components/topBtn";
-import CharDetails from "./pages/charDetails";
+import CharDetails from "./pages/CharDetails.jsx";
 
 function App() {
     return (
