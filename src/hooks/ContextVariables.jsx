816432a73640e26039cab92609791b34fe969ce9
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-29 21:21:42 +0200

    Vite config for 3rd party cookies on development config
    ( on request i can make a 3rd party cookie translator )

--- src/hooks/ContextVariables.jsx
+++ src/hooks/ContextVariables.jsx
@@ -1,14 +1,49 @@
-// Context.js
 import React, { createContext, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({ name: 'Alex', age: 24 });
+    const [user, setUser] = useState({username:`dasd`});
+    
+    async function httpFetch(endpoint, options = {}) {
+    
+        const req = await httpFetchWithCredentials(endpoint, options);
+    
+        if (req.status === 403) {
+            setUser(undefined);
 
-  return (
-    <UserContext.Provider value={user}>
-      {children}
-    </UserContext.Provider>
-  );
-};
+            if (import.meta.env.DEV) {
+            console.warn("Session expired please login again ( 403's expected )\nPlease if you have not been loging on other device you can submit issue in github's issues section!");
+            } else console.log("Session expired please login again ( 403's expected )\nPlease if you have not been loging on other device you can report this issue to GM Lychezar or in Discord!");
+        }
+        
+        return req
+        
+    }
+
+    return (
+        <UserContext.Provider value={{user, setUser, httpFetch}} >
+
+            {children}
+
+        </UserContext.Provider>
+    )
+}
+
+async function httpFetchWithCredentials(endpoint, options = {}) {
+    const apiDomain = import.meta.env.VITE_API_URL;
+
+    const defaultOptions = {
+
+        credentials: "include", // always include cookies
+        headers: {
+          "600": "BasicPass",
+          "Content-Type": "application/json",
+          ...options.headers,
+        },
+    };
+
+    const finalOptions = { ...defaultOptions, ...options };
+
+    return fetch(apiDomain + endpoint, finalOptions)
+}
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  server: {
+    proxy :{
+      "/api" : {
+        target: "https://api.pvpscalpel.com",
+        changeOrigin: true,
+        secure: true
+      }
+
+    }
+  }
 })
