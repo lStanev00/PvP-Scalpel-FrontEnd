27d4f2121771b848d87f0acbd5818e3f4c4b3f02
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-03-31 23:07:57 +0300

    FN renamed

--- src/pages/utility/VlidateToken.jsx
+++ src/pages/utility/VlidateToken.jsx
@@ -1,10 +1,34 @@
 import Style from '../../Styles/modular/Verify.module.css';
 import {  useParams  } from 'react-router-dom';
-import {  useEffect, useRef  } from 'react'; 
+import {  useContext, useEffect, useRef, useState  } from 'react'; 
 import { handleKeydown, handlePaste } from '../../helpers/6digitHandlers';
-export default function VerifyToken() {
+import { UserContext } from '../../hooks/ContextVariables';
+export default function VlidateToken() {
     const { scenario } = useParams();
     const sixDigitRef = useRef([]);
+    const {httpFetch} = useContext(UserContext);
+    const [error, setError] = useState();
+
+    const handleValidatePatch = async ( e ) => {
+        let sixDigitCode = ``;
+        const subURL = `validate/token`
+    
+        for (const el of sixDigitRef.current) {
+            // if (el.value === ``) return
+            sixDigitCode += String(el.value);
+        }
+    
+        const req = await httpFetch(subURL, {
+            method: "PATCH",
+            body: JSON.stringify({
+                token: sixDigitCode,
+                option: scenario
+            })
+        })
+    
+        console.log(req)
+        return req
+    }
 
     useEffect(() => {
         if (sixDigitRef.current.length > 4) {
@@ -21,15 +45,13 @@ export default function VerifyToken() {
         }
     })
 
-    if (scenario == `email`) {
+    if (scenario == `verify`) {
         return (<>
-
-
         <div className={Style.banner}>Verify email</div>
         <section className={Style.container}>
 
             <div>
-                <div>Please fill the boxes with the provided code in your email</div>
+                <div>Please fill the boxes with the provided code in your {scenario}</div>
             </div>
             <br />
             
@@ -102,7 +124,7 @@ export default function VerifyToken() {
                     />
             </div>
 
-            <button type="submit">Verify email</button>
+            <button onClick={async(e) => await handleValidatePatch(e)}>Verify email</button>
 
         </section>
         </>)
