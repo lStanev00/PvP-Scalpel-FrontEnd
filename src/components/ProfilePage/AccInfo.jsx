2f1e26999349d07618fb572ab158884975ecbc6c
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-01 15:21:15 +0300

    New user modify functionality and dom update

--- src/components/ProfilePage/AccInfo.jsx
+++ src/components/ProfilePage/AccInfo.jsx
@@ -1,24 +1,56 @@
+import { useState } from 'react'
+import Style from '../../Styles/modular/AccInfo.module.css'
+import { UsernameForm } from './AccInfoModifyForm';
 export default function AccInfo({user}) {
+    const [editUsername, setEditUsername] = useState(false);
     return (
         <>
         <h2>Account Info</h2>
 
-        <br />
 
-        <div>
-            <strong>Username:</strong> {user.username}
-            <span title="Edit"> ✏️</span>
-        </div>
-        <div>
-            <strong>Email:</strong> {user.email}
-            <span title="Edit"> ✏️</span>
-        </div>
+        <section className={Style.main}>
 
-        <div>
-            <strong>Role:</strong> {user.role}
-        </div>
+            <div className={Style.card}>
 
-        <div>Status:{user.isVerified ? ( <strong> ✅ Verified </strong> ) : ( <strong> ❌ Not Verified </strong> )}</div>
+                <strong>Username:</strong>
+                {!editUsername && (
+                <div>
+                    {user.username}
+                    <span style={{cursor: "pointer"}} onClick={() => setEditUsername(true)} title="Edit"> ✏️ </span>
+                </div>
+                )}
+                {editUsername && (
+                <UsernameForm setEditUsername={setEditUsername} currentUsername={user.username} />
+                )}
+            </div>
+
+            <div className={Style.card}>
+
+                <strong>Email:</strong>
+
+                <div>
+
+                    {user.email}<span title="Edit"> ✏️</span>
+
+                </div>
+
+            </div>
+
+            <div className={Style.card}>
+
+                <strong>Role:</strong>
+
+                <div>
+
+                    {user.role}
+
+                </div>
+
+            </div>
+
+            <div className={Style.card}><strong>Status:</strong>{user.isVerified ? ( <p>✅ Verified</p> ) : ( <p>❌ Not Verified <br /> Please verify your email to be able to use all functionalities</p> )}</div>
+
+        </section>
         </>
     )
 }
---
+++ src/components/ProfilePage/AccInfoModifyForm.jsx
@@ -0,0 +1,55 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../../hooks/ContextVariables';
+
+export function UsernameForm ({currentUsername, setEditUsername}) {
+    const {httpFetch, setUser} = useContext(UserContext);
+    const [error, setError] = useState();
+
+    const handleUsernameModification = async (e) => {
+        e.preventDefault();
+
+        setError(undefined);
+        
+        const newUsername = (new FormData(e.target)).get("username").trim();
+
+        if (!newUsername) return setError(`Please provide username`);
+        if (newUsername.length < 3) return setError(`Username have to be at last 3 symbols long!`);
+
+        try {
+            const req = await httpFetch(`/change/username`, {
+                method: "PATCH",
+                body: JSON.stringify({newUsername}),
+            })
+
+            const status = req.status;
+            if (status == 400) return setError(`Please provide new Username`);
+            if (status == 409) return setError(req.data.msg); 
+            if (status == 500) return setError(`Internal server error! Please report or open issue on github.`); 
+            if (status == 201){
+                setUser(req.data)
+                setTimeout(async () => {
+                    await httpFetch("/verify/me")
+                }, 500);
+
+            }
+        } catch (error) {
+            return setError(`Fetch error: ${error}`);
+        }
+        
+    }
+    return(<>
+        <div>
+            <form style={{gap:`10px`}} onSubmit={async (e) => await handleUsernameModification(e)}>
+
+                <input type="text" name='username' defaultValue={currentUsername} />
+                {error && (
+                    <p style={{color:'red', fontWeight: `bold`}}>{error}</p>
+                )}
+
+                <button type='submit'>Submit</button>
+
+                <button onClick={(e) => {e.preventDefault();setEditUsername(false)}}>Cancel</button>
+            </form>
+        </div>
+    </>)
+}
