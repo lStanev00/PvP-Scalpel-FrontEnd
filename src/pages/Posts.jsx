da675b63360e0f27480b927f40d70a79a85f3838
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-02 18:03:47 +0300

    char image implementation and inline css

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,8 +1,10 @@
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../hooks/ContextVariables";
 import style from '../Styles/modular/Posts.module.css'
+import { useNavigate } from "react-router-dom";
 
 export default function Posts() {
+    const navigate = useNavigate()
     const {httpFetch} = useContext(UserContext);
     const [ posts, setPosts ] = useState(undefined);
 
@@ -24,14 +26,17 @@ export default function Posts() {
         {Object.entries(posts).map(([key, post]) => (
             <div key={post._id} className={style["post-card"]}>
             <div className={style["post-header"]}>
-                <span className={style["author"]}>🧑 {post.author.username}</span>
+                <span className={style["author"]}>Posted by User:🧑 {post.author.username}</span>
                 <span className={style["date"]}>🕓 {new Date(post.createdAt).toLocaleDateString()}</span>
             </div>
             <h3 className={style["title"]}>{post.title}</h3>
             <p className={style["content"]}>{post.content}</p>
             <div className={style["char-meta"]}>
-                <span>🧙‍♂️ {post.character.name}</span>
-                <span>🌍 {post.character.playerRealm.name} / {post.character.server}</span>
+                <span style={{fontSize: "1rem" , display:"flex", flexDirection: "row", alignItems: "center", gap: "0.5rem"}}>
+                    <img style={{borderRadius:"1rem", width: "2rem"}} src={post.character.media.avatar} alt="Character image" /> 
+                    {post.character.name}
+                </span>
+                <span span style={{fontSize: "1rem" , display:"flex", flexDirection: "row", alignItems: "center", gap: "0.5rem"}}>🌍 {post.character.playerRealm.name} / {post.character.server}</span>
             </div>
             </div>
         ))}
