439b9ba7627968aa4324faf4a5ed36bf67eeb652
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-02 23:21:01 +0300

    Error handling

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -24,19 +24,19 @@ export default function Posts() {
     return (<>
     <div className={style["post-grid"]}>
         {Object.entries(posts).map(([key, post]) => (
-            <div onClick={(e) => navigate(`/check/${post.character.server}/${post.character.playerRealm.slug}/${post.character.name}?comment=${post._id}`)} key={post._id} className={style["post-card"]}>
+            <div onClick={(e) => navigate(`/check/${post?.character?.server}/${post?.character?.playerRealm?.slug}/${post?.character?.name}?comment=${post._id}`)} key={post._id} className={style["post-card"]}>
                 <div className={style["post-header"]}>
-                    <span className={style["author"]}>Posted by User:🧑 {post.author.username}</span>
+                    <span className={style["author"]}>Posted by User:🧑 {post?.author?.username}</span>
                     <span className={style["date"]}>🕓 {new Date(post.createdAt).toLocaleDateString()}</span>
                 </div>
-                <h3 className={style["title"]}>{post.title}</h3>
-                <p className={style["content"]}>{post.content}</p>
+                <h3 className={style["title"]}>{post?.title}</h3>
+                <p className={style["content"]}>{post?.content}</p>
                 <div className={style["char-meta"]}>
                     <span style={{fontSize: "1rem" , display:"flex", flexDirection: "row", alignItems: "center", gap: "0.5rem"}}>
-                        <img style={{borderRadius:"1rem", width: "2rem"}} src={post.character.media.avatar} alt="Character image" /> 
-                        {post.character.name}
+                        <img style={{borderRadius:"1rem", width: "2rem"}} src={post?.character?.media?.avatar} alt="Character image" /> 
+                        {post?.character?.name}
                     </span>
-                    <span span style={{fontSize: "1rem" , display:"flex", flexDirection: "row", alignItems: "center", gap: "0.5rem"}}>🌍 {post.character.playerRealm.name} / {post.character.server}</span>
+                    <span span style={{fontSize: "1rem" , display:"flex", flexDirection: "row", alignItems: "center", gap: "0.5rem"}}>🌍 {post?.character?.playerRealm?.name} / {post?.character?.server}</span>
                 </div>
             </div>
         ))}
