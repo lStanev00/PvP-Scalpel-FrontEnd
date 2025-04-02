ecbccb3127d77c7fb25a2f37c8cdefa5c40c0a56
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-02 20:01:12 +0300

    layout draft and basic eddit logic

--- src/components/checkDetails/PostTemplate.jsx
+++ src/components/checkDetails/PostTemplate.jsx
@@ -1,9 +1,11 @@
 import postStyle from '../../Styles/modular/PostTemplate.module.css'
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "./../../hooks/ContextVariables";
+import editStyle from '../../Styles/modular/NewPostForm.module.css'
 
 export default function PostTemplate({ post, optimistic, setPosts }) {
   const { user, httpFetch } = useContext(UserContext);
+  const [ eddit, setEddit ] = useState(true);
 
   const isOwner = user?._id === post?.author?._id;
 
@@ -39,21 +41,67 @@ export default function PostTemplate({ post, optimistic, setPosts }) {
         pointerEvents: "none",
       } : {}} 
       >
-      <div className={postStyle["post-meta"]}>
-        <span>🧑 <strong>{post?.author?.username || "Anonymous"}</strong></span>
-        <span>📅 {new Date(post?.createdAt).toLocaleDateString()}</span>
-      </div>
+        {eddit == undefined && (
+            <>
+            <div className={postStyle["post-meta"]}>
+              <span>🧑 <strong>{post?.author?.username || "Anonymous"}</strong></span>
+              <span>📅 {new Date(post?.createdAt).toLocaleDateString()}</span>
+            </div>
+      
+            <div className={postStyle["post-content"]}>
+              <strong>{post?.title || "Untitled Post"}</strong>
+              <span>{post?.content || "No content provided."}</span>
+            </div>
+            
+            {isOwner && (
+              <div className={postStyle["post-actions"]}>
+                <button onClick={(e) => {e.preventDefault(); setEddit(true)}}>📝 Edit</button>
+                <button onClick={ async (e) => await onDelete(e)}>🗑️ Delete</button>
+              </div>
+            )}
+          </>
+        )}
 
-      <div className={postStyle["post-content"]}>
-        <strong>{post?.title || "Untitled Post"}</strong>
-        <span>{post?.content || "No content provided."}</span>
-      </div>
+      {eddit != undefined && (
+        <>
+        <form style={{maxWidth:"350px"}} className={editStyle.form}>
+                <h3 className={editStyle.heading}>Eddit Comment</h3>
+          
+                <input
+                  type="text"
+                  placeholder="Post title"
+                  className={editStyle.input}
+                  required
+                  defaultValue={post.title}
+                  name='title'
+                />
+          
+                <textarea
+                  style={{maxWidth:"320px"}}
+                  placeholder="Write your content..."
+                  className={editStyle.textarea}
+                  required
+                  defaultValue={post.content}
+                  name='content'
+                />
+          
+              {/* {error && (
+                  <>
+                  <p style={{color:"red"}}>{error}</p>
+                  </>
+              )} */}
 
-      {isOwner && (
-        <div className={postStyle["post-actions"]}>
-          <button onClick={() => onEdit(post)}>📝 Edit</button>
-          <button onClick={ async (e) => await onDelete(e)}>🗑️ Delete</button>
-        </div>
+                <div style={{display:"flex", gap:`1rem`}}>
+                <button type="submit" className={editStyle.button}>
+                 Confirm Eddit
+                </button>
+                <button onClick={(e) => {e.preventDefault();setEddit(undefined)}}  className={editStyle.button}>
+                  Cancel
+                </button>
+                </div>
+        </form>
+        
+        </>
       )}
     </div>
   );
