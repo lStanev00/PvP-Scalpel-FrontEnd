1f4573efbbf864d9f30e3a9dcf287a956aa68d4e
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-02 15:17:50 +0300

    Basic nav menu draft for loged/guest views

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,12 +1,25 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from '../hooks/ContextVariables';
 
 export default function Navigation() {
+    const { user } = useContext(UserContext);
     return (
         <>
             <nav className='navbar'>
                 <ul className='nav-links'>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/roster">Roster List</Link></li>
+                    {!user?._id && (
+                        <>
+                            <li><Link to="/login">Login</Link></li>
+                        </>
+                    )}
+                    {user._id && (
+                        <>
+                            <li><Link to="/profile">Profile</Link></li>
+                        </>
+                    )}
+                    <li><Link to="/">Posts</Link></li>
+                    <li><Link to="/roster">Members</Link></li>
                     <li><Link to="/leaderboard">Leaderboard</Link></li>
                 </ul>
             </nav>
