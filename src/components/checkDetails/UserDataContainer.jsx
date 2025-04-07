2513837a1ce2ad9ab94c492624f25f6cf124c748
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-07 17:52:03 +0300

    error handling for not loged user

--- src/components/checkDetails/UserDataContainer.jsx
+++ src/components/checkDetails/UserDataContainer.jsx
@@ -1,21 +1,25 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../hooks/ContextVariables";
 import { CharacterContext } from "../../pages/CharDetails";
 import Style from '../../Styles/modular/charDetails.module.css';
+import { useNavigate } from "react-router-dom";
 
 
 export default function UserDataContainer() {
+    const navigate = useNavigate();
     const { user, httpFetch} = useContext(UserContext);
-    const {data} = useContext(CharacterContext);
-    const [ isLiked, setIsLiked ] = useState((data.likes).includes(user._id));
+    const {data, location} = useContext(CharacterContext);
+    const [ isLiked, setIsLiked ] = useState();
+
+    useEffect(() => {   if (user?._id) setIsLiked((data?.likes).includes(user._id))   }, [])
 
     const likeHandler = async (e) => {
         e.preventDefault();
         const likeURL = `/like/${data._id}`
-        console.log((data.likes).includes(user._id))
         const req = await httpFetch(likeURL);
 
-        console.log(req.data)
+        console.log(req.status);
+        if(req.status == 401) return navigate(`/login?target=${location}`)
         setIsLiked(req.data?.isLiked);
     }
 
@@ -32,11 +36,11 @@ export default function UserDataContainer() {
                 <span>{data.race} | Level {data.level} | {data.class.name} ({data.activeSpec.name})</span>
             </div>
             
-            <img src= "/user_action_icons/Like.png" alt="Character Avatar" />
+            {/* <img src= "/user_action_icons/Like.png" alt="Character Avatar" />
             <div className={Style["banner-content"]}>
                 <strong>{data.name} - {data.playerRealm.name}</strong>
                 <span>{data.race} | Level {data.level} | {data.class.name} ({data.activeSpec.name})</span>
-            </div>
+            </div> */}
             {/* <ReloadBTN setData={setData} data={data} isUpdating={isUpdating} setUpdating={setUpdating} /> */}
 
     </div>
