e2838a70c805dea420fc2d605460bcdb1cea9935
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-10 15:32:04 +0300

    Data processing

--- src/components/checkDetails/StatsChart.jsx
+++ src/components/checkDetails/StatsChart.jsx
@@ -5,10 +5,27 @@ import { CharacterContext } from '../../pages/CharDetails';
 
 export default function StatsChart () {
     const {data} = useContext(CharacterContext);
-    const userStats = data.equipmentStats;
+    const rawUserStats = data.equipmentStats;
+
+    const stats = {
+        main: {
+            [rawUserStats.Primary[0]]: rawUserStats.Primary[1],
+            Stamina: rawUserStats.Stamina,
+            Armor: rawUserStats.Armor
+        },
+        secondary: {
+            Versatility: rawUserStats.Versatility,
+            CriticalStrike: rawUserStats.CriticalStrike,
+            Haste: rawUserStats.Haste,
+            Mastery: rawUserStats.Mastery,
+        },
+    };
+
+    
     return (
         <section className={Style.section}>
             <h2>Stats Priority</h2>
+
             <div>
 
                 
