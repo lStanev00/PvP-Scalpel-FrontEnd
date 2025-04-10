4bf956440babab275b7794a9d5422d54c7e31812
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-04-10 17:54:02 +0300

    Basic idea of how the graph will look

--- src/components/checkDetails/StatsChart.jsx
+++ src/components/checkDetails/StatsChart.jsx
@@ -13,21 +13,41 @@ export default function StatsChart () {
             Stamina: rawUserStats.Stamina,
             Armor: rawUserStats.Armor
         },
-        secondary: {
-            Versatility: rawUserStats.Versatility,
-            CriticalStrike: rawUserStats.CriticalStrike,
-            Haste: rawUserStats.Haste,
-            Mastery: rawUserStats.Mastery,
-        },
+        secondary: [
+            ["Versatility", rawUserStats.Versatility],
+            ['Critical Strike', rawUserStats.CriticalStrike],
+            ["Haste", rawUserStats.Haste],
+            ['Mastery', rawUserStats.Mastery],
+        ],
     };
+
+    stats.secondary = stats.secondary.sort((a, b) => {
+        const valA = Number(a[1].replace(`%`, ``));
+        const valB = Number(b[1].replace(`%`, ``));
+        return valB - valA; 
+    });
+    
 
     
     return (
         <section className={Style.section}>
             <h2>Stats Priority</h2>
 
-            <div>
+            <div className={Style.graph}>
 
+                <div>
+                    <h3>{stats.secondary[0][0]}</h3>
+                    <div  style={{height: "50px", backgroundColor:'red'}}>D</div>
+                </div>
+                <div>
+                    <h3>{stats.secondary[1][0]}</h3>
+                </div>
+                <div>
+                    <h3>{stats.secondary[2][0]}</h3>
+                </div>
+                <div>
+                    <h3>{stats.secondary[3][0]}</h3>
+                </div>
                 
             </div>
         </section>
