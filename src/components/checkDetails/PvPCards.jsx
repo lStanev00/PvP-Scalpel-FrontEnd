c9be2a22c082580dd72b97b25579fe3f0e45a5fb
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-05 13:01:30 +0300

    clear console log

--- src/components/checkDetails/PvPCards.jsx
+++ src/components/checkDetails/PvPCards.jsx
@@ -1,7 +1,6 @@
 import TableStyle from '../../Styles/modular/checkDetailsPvPTable.module.css'
 export default function PvPCards({ title, bracketData, Style }) {
     if (bracketData?.currentSeason?.rating == 0) bracketData.currentSeason.rating = null;
-    console.log(bracketData)
     return (
             <div className={Style["pvp-card"]}>
                 <section className={Style["inner-section"]}>
