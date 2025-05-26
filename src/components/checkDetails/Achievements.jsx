abefad7151c416fe9cf64db4c436b64c56cd1347
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-05-26 17:55:35 +0300

    First draft of the Achievements section

---
+++ src/components/checkDetails/Achievements.jsx
@@ -0,0 +1,26 @@
+import { useContext, useState } from "react";
+import { CharacterContext } from "../../pages/CharDetails";
+import Style from "../../Styles/modular/AchSection.module.css"
+
+export default function AchevementSection() {
+    const {data} = useContext(CharacterContext);
+    const [achievements, setAchievements] = useState(data.achieves);
+    console.log(achievements);
+
+
+    return(
+    <>
+        <div className={Style["section"]}>
+            <h1>Achievements ({data.achieves.points} Points)</h1>
+            <div className={Style["card"]}>
+                <img src="https://render.worldofwarcraft.com/eu/icons/56/achievement_arena_2v2_4.jpg" alt="Achievement" />
+                <div className={Style["card-content"]}>
+                    <strong>Just the Two of Us: 1750</strong>
+                    <span>Earn a 1750 personal rating in the 2v2 bracket of the arena.</span>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+
+}
