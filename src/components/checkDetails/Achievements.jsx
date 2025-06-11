13612a6a405c4fe718fab5ba5dd18530b7f11580
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-11 11:17:05 +0300

    solving key problems

--- src/components/checkDetails/Achievements.jsx
+++ src/components/checkDetails/Achievements.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { CharacterContext } from "../../pages/CharDetails";
 import Style from "../../Styles/modular/AchSection.module.css"
 import filterAchieves from "../../helpers/achviesCheckers.js";
+import { v4 as uuidv4 } from 'uuid';
 
 export default function AchevementsSection() {
     const {data} = useContext(CharacterContext);
@@ -27,7 +28,7 @@ export default function AchevementsSection() {
                     shadowSeasonMap.set("noSeason", fallback);
                 }
             }
-            
+
             const sortedAches = filterAchieves(shadowSeasonMap);
             if (sortedAches) setSeasonalAchives(sortedAches)
             
@@ -56,11 +57,9 @@ export default function AchevementsSection() {
                 }
 
                 if(value.XP || value.WINS) {
-                    return (<>
-                        {Object.entries(value).map(([achKey, achData]) => {
-                            return(<AchievementDiv key={key +  achKey} achData={achData} />)
-                        })}
-                    </>)
+                    return Object.entries(value).map(([achKey, achData]) => (
+                        <AchievementDiv key={`${key}-${achKey}`} achData={achData} />
+                    ));
                 }
 
                 if(value.name)return <AchievementDiv key={ key } achData={value} />
@@ -76,18 +75,32 @@ export default function AchevementsSection() {
                 {seasonalAchives.size !== 0 && (
                     Array.from(seasonalAchives.entries()).map(([key, value]) => {
                         if (key === "noSeason") return null;
-                        return (<>
-                        <div className={Style.seasonalMain}>
+                        return (
+                        <div key={uuidv4()} className={Style.seasonalMain}>
                             <h2>{key}</h2>
                             <div className={Style.seasonalAchieves}>
                                 {value && Object.entries(value).map(([seasonIndex, achList]) => (
-                                    achList.map(ach => (
-                                        <AchievementDiv key={ach.criteria || ach.name} seasonal={true} achData={ach} />
-                                    ))
+                                    achList.map(ach => {
+
+                                        if(!ach.criteria) {
+                                            <AchievementDiv key={uuidv4()} seasonal={true} achData={ach} />
+
+                                        }
+
+                                        try {
+                                            
+                                            return (
+                                                <AchievementDiv key={(ach._id || ach.criteria || ach.name).replace(/\s+/g, "-")} seasonal={true} achData={ach} />
+                                            )
+                                        } catch (error) {
+                                        return <AchievementDiv key={ach._id ||ach.criteria} seasonal={true} achData={ach} />
+
+                                        }
+
+                                    })
                                 ))}
                             </div>
                         </div>
-                        </>
                         );
                     })
                 )}
@@ -103,7 +116,6 @@ export default function AchevementsSection() {
 function AchievementDiv({achData, seasonal = undefined}){
 
     if(achData) return (
-    <>
         <div className={Style[seasonal == undefined? "card": `seasonal-card`]}>
             <img src={achData.media} alt="Achievement Image" />
             <div className={Style[seasonal == undefined? "card-content": "seasonal-card-content"]}>
@@ -111,6 +123,5 @@ function AchievementDiv({achData, seasonal = undefined}){
                 <span>{achData.description}</span>
             </div>
         </div>
-    </>
     )
 }
