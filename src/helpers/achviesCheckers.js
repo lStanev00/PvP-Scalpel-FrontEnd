5873bd3e437e1081046cab3aaccaf8a8e3d71287
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-11 10:21:36 +0300

    sorting by freshes expansion

--- src/helpers/achviesCheckers.js
+++ src/helpers/achviesCheckers.js
@@ -36,12 +36,43 @@ export default function filterAchieves(mapArr) {
                 const titleIndex = ssAches.findIndex(ach => ach.name.includes(title));
                 if(titleIndex != -1) biggest = ssAches.splice(titleIndex, 1)[0]
             }
-            
+
             if(biggest != null) ssAches.push(biggest)
         }
 
     }
 
-    return mapArr
+    const wowExpansions = [
+    "noSeason",
+    undefined,
+    "World of Warcraft",
+    "The Burning Crusade",
+    "Wrath of the Lich King",
+    "Cataclysm",
+    "Mists of Pandaria",
+    "Warlords",
+    "Legion",
+    "Battle for Azeroth",
+    "Shadowlands",
+    "Dragonflight",
+    "The War Within",
+    "Midnight",
+    "The Last Titan"
+    ].reverse();
+
+
+    const sortedMap = new Map([...mapArr.entries()].sort((a, b) => {
+        const expansion1 = a[0];
+        const expansion2 = b[0];
+
+        const hierarchyIndex1 = wowExpansions.indexOf(expansion1);
+        const hierarchyIndex2 = wowExpansions.indexOf(expansion2);
+        
+        return hierarchyIndex1 - hierarchyIndex2;
+    }))
+    
+    console.log(sortedMap)
+
+    return sortedMap
 
 }
