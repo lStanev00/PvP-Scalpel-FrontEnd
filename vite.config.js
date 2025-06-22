44d33e0521041e8098a760a981f5a2c9be039e25
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-06-22 11:28:53 +0300

    rewind

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    base: "/",
-    server: {
-        proxy: {
-            "/api": {
-                target: "https://api.pvpscalpel.com",
-                changeOrigin: true,
-                secure: true,
-                rewrite: (path) => path.replace(/^\/api/, "")
-            }
-        }
-    },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
-            'src': path.resolve(__dirname, './src'),
-        },
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
+  plugins: [react()],
+  base: "/",
+  server: {
+    proxy :{
+      "/api" : {
+        target: "https://api.pvpscalpel.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, "")
+      }
+
     }
+  },
+  
 })
