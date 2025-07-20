86a1fcf34b00fd1666456846ec306114333d3100
Lachezar Stanev <159606254+lStanev00@users.noreply.github.com>
2025-07-20 12:20:49 +0300

    DropDown item logic

--- src/components/SearchBar/components/DropDownItem.jsx
+++ src/components/SearchBar/components/DropDownItem.jsx
@@ -1,9 +1,31 @@
-export default function DropDownItem ({entry, Style}) {
+import { useNavigate } from "react-router-dom"
+
+export default function DropDownItem ({entry, Style, inputRef}) {
+    const navigate = useNavigate();
+    const char = entry.char;
+
+    const handleClick = () => {
+        inputRef.current.value = ""
+        navigate(`/check/${char.server}/${char.playerRealm.slug}/${char.name}`)
+    }
     return (
 
-    <li className={Style.dropdownItem}>
-        <img className={Style.classIcon} src={entry.char.class.media} alt={entry.char.class.name + "class icon"} />
-        {entry?.char?.name} - {entry?.realmName}
+    <li
+        onClick={() => handleClick()}
+        className={Style.dropdownItem}
+    >
+
+        <img className={Style.classIcon} src={char.class.media} alt={char.class.name + "class icon"} />
+        
+        <div 
+            className={Style.playerData}
+        >
+            <p className={Style.nameRealm}>
+
+                {char?.name} - {entry?.realmName}
+            </p>
+            <span className={Style.serverName}>{(char.server).toUpperCase()}</span>
+        </div>
 
     </li>
 
